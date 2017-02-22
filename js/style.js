d081f21a962e8bd27c45255e734389dde3c8daf8
getveloce <office@getveloce.com>
2017-02-22 22:12:24 +0100

    new placement widgets

--- js/style.js
+++ js/style.js
@@ -20,6 +20,7 @@ $(document).ready(function(){
         var x0 = 0;
         var x_offset = 35;
         var x_offset_3dviewer = 60;
+        var x_offset_axes = 90;
         var y0 = 70;
 
         var window_widget_spjs_draggable = [x0, y0, $( window ).width() - $( "#widget-spjs-container" ).width() - x0 - x_offset, $( window ).height() - y0];
@@ -28,7 +29,7 @@ $(document).ready(function(){
         var window_widget_console_draggable = [x0, y0, $( window ).width() - $( "#widget-console-container" ).width() - x0 - x_offset, $( window ).height() - y0];
         $( "#widget-console-container" ).draggable({ handle: ".widget-drag-handle", containment: window_widget_console_draggable, stack: ".draggable" });
 
-        var window_widget_axes_draggable = [x0, y0, $( window ).width() - $( "#widget-axes-container" ).width() - x0 - x_offset, $( window ).height() - y0];
+        var window_widget_axes_draggable = [x0 + x_offset_axes, y0, $( window ).width() - $( "#widget-axes-container" ).width() - x0 - x_offset, $( window ).height() - y0];
         $( "#widget-axes-container" ).draggable({ handle: ".widget-drag-handle", containment: window_widget_axes_draggable , stack: ".draggable" });
 
         var window_widget_3dviewer_draggable = [x0, y0, $( window ).width() - $( "#widget-3dviewer-container" ).width() - x0 - x_offset_3dviewer, $( window ).height() - y0];
@@ -39,15 +40,15 @@ $(document).ready(function(){
      * Make widgets visible / invisible
      **/
     $( "#hide-show-widget-spjs" ).on( "click", function(event) {
-         $( "#widget-spjs-container" ).toggle( "show" );
+        $( "#widget-spjs-container" ).toggle( "show" );
     });
 
     $( "#hide-show-widget-console" ).on( "click", function(event) {
-         $( "#widget-console-container" ).toggle( "show" );
+        $( "#widget-console-container" ).toggle( "show" );
     });
 
     $( "#hide-show-widget-axes" ).on( "click", function(event) {
-         $( "#widget-axes-container" ).toggle( "show" );
+        $( "#widget-axes-container" ).toggle( "show" );
     });
 
     $( "#hide-show-widget-3dviewer" ).on( "click", function(event) {
