ebbef66befa1b7aca01b5a044ca78eab0f70c509
getveloce <office@getveloce.com>
2017-02-23 10:39:40 +0100

    test 3d viewer #10

--- js/style.js
+++ js/style.js
@@ -23,36 +23,25 @@ $(document).ready(function(){
         var x_offset_axes = 90;
         var y0 = 70;
 
-        var stack_index = 100;
-
         var window_widget_spjs_draggable = [x0, y0, $( window ).width() - $( "#widget-spjs-container" ).width() - x0 - x_offset, $( window ).height() - y0];
         $( "#widget-spjs-container" ).draggable({
             handle: ".widget-drag-handle",
             containment: window_widget_spjs_draggable,
-            //stack: ".draggable"
-            start: function( event, ui ) {
-                $( "#widget-spjs-container" ).css( "z-index", stack_index++ );
-            }
+            stack: ".draggable"
           });
 
         var window_widget_console_draggable = [x0, y0, $( window ).width() - $( "#widget-console-container" ).width() - x0 - x_offset, $( window ).height() - y0];
         $( "#widget-console-container" ).draggable({
             handle: ".widget-drag-handle",
             containment: window_widget_console_draggable,
-            //stack: ".draggable"
-            start: function( event, ui ) {
-                $( "#widget-console-container" ).css( "z-index", stack_index++ );
-            }
+            stack: ".draggable"
           });
 
         var window_widget_axes_draggable = [x0 + x_offset_axes, y0, $( window ).width() - $( "#widget-axes-container" ).width() - x0 - x_offset, $( window ).height() - y0];
         $( "#widget-axes-container" ).draggable({
             handle: ".widget-drag-handle",
             containment: window_widget_axes_draggable,
-            //stack: ".draggable"
-            start: function( event, ui ) {
-                $( "#widget-axes-container" ).css( "z-index", stack_index++ );
-            }
+            stack: ".draggable"
           });
 
         var window_widget_3dviewer_draggable = [x0, y0, $( window ).width() - $( "#widget-3dviewer-container" ).width() - x0 - x_offset_3dviewer, $( window ).height() - y0];
@@ -60,8 +49,12 @@ $(document).ready(function(){
             handle: ".widget-drag-handle",
             containment: window_widget_3dviewer_draggable,
             start: function( event, ui ) {
-                $( "#widget-3dviewer-panel-heading" ).css( "z-index", stack_index++ );
-            }
+                $( "#widget-3dviewer-panel-heading" ).css( "z-index", "1000" );
+                $( "#widget-3dviewer-panel-heading" ).css( "position", "absolute" );
+            },
+            stop: function( event, ui ) {
+                $( "#widget-3dviewer-panel-heading" ).css( "z-index", "1" );
+            },
           });
     } );
 
